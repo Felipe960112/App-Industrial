13c563d54ae51b90c3ca2fc812ac4182230ca9b0
Felipe Valencia <pipevalencia0112@hotmail.com>
2018-08-10 00:45:08 -0500

    Se termina la version 2 del proyecto.

--- Assets/Scripts/Ejercicio1Manager.cs
+++ Assets/Scripts/Ejercicio1Manager.cs
@@ -21,6 +21,21 @@ public class Ejercicio1Manager : MonoBehaviour {
     public Text Ganancias;
 
     public AudioClip sonidoCrear;
+    public GameObject Mesa1;
+    public GameObject Mesa2;
+    public GameObject Mesa3;
+    public GameObject Mesa4;
+    public GameObject Mesa5;
+    public GameObject Mesa6;
+    public GameObject Silla1;
+    public GameObject Silla2;
+    public GameObject Silla3;
+    public GameObject Silla4;
+    public GameObject Silla5;
+    public GameObject Silla6;
+    public GameObject Silla7;
+    public GameObject Silla8;
+
 
 
     AudioSource fuenteSonidoCrear;
@@ -48,24 +63,167 @@ public class Ejercicio1Manager : MonoBehaviour {
         Ganancias.text = "$" + ganancias.ToString();
 
 
-        //------Aqui debo verificar que hay cantidades disponibles, sumas y otros
+        Mesa1.SetActive(false);
+        Mesa2.SetActive(false);
+        Mesa3.SetActive(false);
+        Mesa4.SetActive(false);
+        Mesa5.SetActive(false);
+        Mesa6.SetActive(false);
+
+        Silla1.SetActive(false);
+        Silla2.SetActive(false);
+        Silla3.SetActive(false);
+        Silla4.SetActive(false);
+        Silla5.SetActive(false);
+        Silla6.SetActive(false);
+        Silla7.SetActive(false);
+        Silla8.SetActive(false);
 
 
+        if (cantMesas == 1)
+        {
+            Mesa1.SetActive(true);
+            Mesa2.SetActive(false);
+            Mesa3.SetActive(false);
+            Mesa4.SetActive(false);
+            Mesa5.SetActive(false);
+            Mesa6.SetActive(false);
+        }
+        else if (cantMesas == 2)
+        {
+            Mesa1.SetActive(true);
+            Mesa2.SetActive(true);
+            Mesa3.SetActive(false);
+            Mesa4.SetActive(false);
+            Mesa5.SetActive(false);
+            Mesa6.SetActive(false);
+        }
+        else if (cantMesas == 3)
+        {
+            Mesa1.SetActive(true);
+            Mesa2.SetActive(true);
+            Mesa3.SetActive(true);
+            Mesa4.SetActive(false);
+            Mesa5.SetActive(false);
+            Mesa6.SetActive(false);
+        }
+        else if (cantMesas == 4)
+        {
+            Mesa1.SetActive(true);
+            Mesa2.SetActive(true);
+            Mesa3.SetActive(true);
+            Mesa4.SetActive(true);
+            Mesa5.SetActive(false);
+            Mesa6.SetActive(false);
+        }
+        else if (cantMesas ==5)
+        {
+            Mesa1.SetActive(true);
+            Mesa2.SetActive(true);
+            Mesa3.SetActive(true);
+            Mesa4.SetActive(true);
+            Mesa5.SetActive(true);
+            Mesa6.SetActive(false);
+        }
+        else if (cantMesas == 6)
+        {
+            Mesa1.SetActive(true);
+            Mesa2.SetActive(true);
+            Mesa3.SetActive(true);
+            Mesa4.SetActive(true);
+            Mesa5.SetActive(true);
+            Mesa6.SetActive(true);
+        }
 
 
-        //if (vidas <= 0)
-        //{
-        //    SceneManager.LoadScene("Menu");
-        //}
-
-        //winBox.SetActive(false);
-
-        //if (monedas == 3)
-        //{
-        //    winBox.SetActive(true);
-        //}
-
-        //VidasNivel.text = "Vidas restantes : " + vidas.ToString();
+        if (cantSillas == 1)
+        {
+            Silla1.SetActive(true);
+            Silla2.SetActive(false);
+            Silla3.SetActive(false);
+            Silla4.SetActive(false);
+            Silla5.SetActive(false);
+            Silla6.SetActive(false);
+            Silla7.SetActive(false);
+            Silla8.SetActive(false);
+        }
+        else if (cantSillas == 2)
+        {
+            Silla1.SetActive(true);
+            Silla2.SetActive(true);
+            Silla3.SetActive(false);
+            Silla4.SetActive(false);
+            Silla5.SetActive(false);
+            Silla6.SetActive(false);
+            Silla7.SetActive(false);
+            Silla8.SetActive(false);
+        }
+        else if (cantSillas == 3)
+        {
+            Silla1.SetActive(true);
+            Silla2.SetActive(true);
+            Silla3.SetActive(true);
+            Silla4.SetActive(false);
+            Silla5.SetActive(false);
+            Silla6.SetActive(false);
+            Silla7.SetActive(false);
+            Silla8.SetActive(false);
+        }
+        else if (cantSillas == 4)
+        {
+            Silla1.SetActive(true);
+            Silla2.SetActive(true);
+            Silla3.SetActive(true);
+            Silla4.SetActive(true);
+            Silla5.SetActive(false);
+            Silla6.SetActive(false);
+            Silla7.SetActive(false);
+            Silla8.SetActive(false);
+        }
+        else if (cantSillas == 5)
+        {
+            Silla1.SetActive(true);
+            Silla2.SetActive(true);
+            Silla3.SetActive(true);
+            Silla4.SetActive(true);
+            Silla5.SetActive(true);
+            Silla6.SetActive(false);
+            Silla7.SetActive(false);
+            Silla8.SetActive(false);
+        }
+        else if (cantSillas == 6)
+        {
+            Silla1.SetActive(true);
+            Silla2.SetActive(true);
+            Silla3.SetActive(true);
+            Silla4.SetActive(true);
+            Silla5.SetActive(true);
+            Silla6.SetActive(true);
+            Silla7.SetActive(false);
+            Silla8.SetActive(false);
+        }
+        else if (cantSillas == 7)
+        {
+            Silla1.SetActive(true);
+            Silla2.SetActive(true);
+            Silla3.SetActive(true);
+            Silla4.SetActive(true);
+            Silla5.SetActive(true);
+            Silla6.SetActive(true);
+            Silla7.SetActive(true);
+            Silla8.SetActive(false);
+        }
+        else if (cantSillas == 8)
+        {
+            Silla1.SetActive(true);
+            Silla2.SetActive(true);
+            Silla3.SetActive(true);
+            Silla4.SetActive(true);
+            Silla5.SetActive(true);
+            Silla6.SetActive(true);
+            Silla7.SetActive(true);
+            Silla8.SetActive(true);
+        }
 
 
     }
